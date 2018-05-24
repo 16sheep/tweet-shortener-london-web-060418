3da6803643fdba857ca21ce07b5fc4cad82fa9e2
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-24 13:36:32 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+def dictionary 
+  dictionary_hash = {"hi" => ["hello"], 
+  "2" => ["to", "two", "too"],
+  "4" => ["for", "four"],
+  "b" => ["be"],
+  "@" => ["at"],
+  "&" => ["and"]
+  }
+end
+
+def word_substituter(tweet){
+  tweet = tweet.split 
+  print tweet
+}
