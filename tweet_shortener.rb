a2495146433037395a4326a3c697eb5e037e965c
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-29 05:42:47 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,3 @@
-# Write your code here.
-
 def dictionary 
   dictionary_hash = {"hi" => ["hello"], 
   "2" => ["to", "two", "too"],
@@ -10,7 +8,29 @@ def dictionary
   }
 end
 
-def word_substituter(tweet){
-  tweet = tweet.split 
-  print tweet
-}
+def word_substituter(tweet)
+  dic = dictionary.keys
+  newtweet = tweet.split
+  values = dictionary.values
+  
+  print "This is tweet #{newtweet}"
+  print "This is keys #{dic}"
+  print "This is values #{values}"
+  
+  
+  
+  
+  for word in newtweet
+  
+    lasttweet = []
+    for x in values
+      for y in x
+        y == word ? lasttweet.push(dic[values.index(x)]) : lasttweet.push(word)
+      end
+    end
+  end
+
+  lasttweet = lasttweet.join(" ")
+  lasttweet
+  
+end
